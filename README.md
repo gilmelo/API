802f2a15dbd57c14c36c321ac08cce9928c6c503
Gilson Teixeira de Melo <135767363+gilmelo@users.noreply.github.com>
2023-11-30 23:07:58 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# API
+# API-Fake_Store
---
+++ js/script.js
@@ -0,0 +1,7 @@
+const API =('https://fakestoreapi.com');
+
+
+
+fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>console.log(json))
